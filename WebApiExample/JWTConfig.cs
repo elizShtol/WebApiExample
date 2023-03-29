2b38587db4b4d9f0fa4a11ac95774c72d362f992
elizShtol <eliz.shtol@gmail.com>
2023-03-29 23:58:09 +0500

    add authentication controller

---
+++ WebApiExample/JWTConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiExample;
+
+public class JWTConfig
+{
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string JwtKey { get; set; }
+}
--- WebApiExample/Program.cs
+++ WebApiExample/Program.cs
@@ -1,3 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using WebApiExample;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,43 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("JwtConfig.json",
+    optional: true,
+    reloadOnChange: true);
+
+
+var jwtConfig = new JWTConfig();
+builder.Configuration.Bind("Jwt", jwtConfig);    
+builder.Services.AddSingleton(jwtConfig);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,
+        options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                // укзывает, будет ли валидироваться издатель при валидации токена
+                ValidateIssuer = true,
+                // строка, представляющая издателя
+                ValidIssuer = jwtConfig.Issuer,
+
+                // будет ли валидироваться потребитель токена
+                ValidateAudience = true,
+                // установка потребителя токена
+                ValidAudience = jwtConfig.Audience,
+                // будет ли валидироваться время существования
+                ValidateLifetime = true,
+
+                // установка ключа безопасности
+                IssuerSigningKey = new SymmetricSecurityKey
+                    (Encoding.UTF8.GetBytes(jwtConfig.JwtKey)),
+                // валидация ключа безопасности
+                ValidateIssuerSigningKey = true,
+            };
+        });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +60,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
